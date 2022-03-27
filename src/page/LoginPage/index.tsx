fe395c1c16d11af91d228fe32a56714a92e2f504
Moonchop <moonchop913@gmail.com>
2022-03-28 02:05:52 +0900

    saving loginInfo in account-state

--- src/page/LoginPage/index.tsx
+++ src/page/LoginPage/index.tsx
@@ -1,59 +1,13 @@
 import React from "react";
-import "antd/dist/antd.css";
-import { Form, Input, Button, Checkbox } from "antd";
-import styled from "styled-components";
-const onFinish = (values: any) => {
-  console.log("Success:", values);
-};
+import LoginWidget from "../../widget/LoginWidget";
 
-const onFinishFailed = (errorInfo: any) => {
-  console.log("Failed:", errorInfo);
-};
+
 export default function LoginPage() {
- 
+
   return (
     <div>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{ offset: 8, span: 16 }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+      <LoginWidget/> 
     </div>
   );
 }
-
 
--- src/widget/LoginWidget/index.tsx
+++ src/widget/LoginWidget/index.tsx
@@ -1,7 +1,85 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import "antd/dist/antd.css";
+import { Form, Input, Button, Checkbox } from "antd";
+import styled from "styled-components";
+
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
 
 export default function LoginWidget() {
+
+  const [account, setAccount] = useState({
+    id: "",
+    password: "",
+  });
+  const onChangeAccount=(e:any)=>{
+    setAccount({
+      ...account,
+      [e.target.name]:e.target.value,
+    });
+    
+  };
+  console.log(account)
   return (
-    <div>LoginWidget</div>
-  )
+    <div>
+      <Forms
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input name="id" onChange={onChangeAccount} />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password name="password"onChange={onChangeAccount}/>
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkboxs id="checkbox">Remember me</Checkboxs>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Forms>
+    </div>
+  );
 }
+
+const Forms = styled(Form)`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 80vh;
+`;
+
+const Checkboxs = styled(Checkbox)`
+  width: 300px;
+  font-weight: 600;
+`;
