7c84711af89de58ffca3267286954a6e2445b797
Moonchop <moonchop913@gmail.com>
2022-04-09 15:58:52 +0900

    add : admin reducer

--- src/modules/admin.ts
+++ src/modules/admin.ts
@@ -2,3 +2,38 @@ import { createSlice, PayloadAction , createReducer } from "@reduxjs/toolkit";
 
 //admin : id, password, nickname, phone number, email
 //company : company, address, company number, ceo 
+
+export interface IAdmin{
+    id : string;
+    password : string;
+    nickname : string;
+    phoneNum : string;
+    email : string;
+    companyName : string;
+    address : string;
+    companyPhoneNum : string;
+    ceoName : string;
+}
+// export interface ICompany{
+// }
+
+interface IState{
+    adminInfo : IAdmin[];
+}
+const initialState:IState={
+    adminInfo:[]
+};
+
+export const adminSlice = createSlice({
+    name:'admin',
+    initialState,
+    reducers:{
+        addInfo(state,action:PayloadAction<IAdmin>){
+            state.adminInfo.push(action.payload);
+        }
+    },
+});
+
+const {reducer, actions} = adminSlice;
+export const {addInfo} = actions;
+export default reducer;
