c7f32df19353c58e1386a8d16d95771f51623f8b
Moonchop <moonchop913@gmail.com>
2022-04-14 00:03:28 +0900

    add : DaumPostCode

--- src/page/RegisterPage/index.tsx
+++ src/page/RegisterPage/index.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import DaumPostCode from 'react-daum-postcode';
+// import ReCAPTCHA from 'react-google-recaptcha';
 import {
   Form,
+  Modal,
   Input,
   InputNumber,
   Cascader,
@@ -14,40 +17,7 @@ import {
 
 const { Option } = Select;
 
-const residences = [
-  {
-    value: 'zhejiang',
-    label: 'Zhejiang',
-    children: [
-      {
-        value: 'hangzhou',
-        label: 'Hangzhou',
-        children: [
-          {
-            value: 'xihu',
-            label: 'West Lake',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: 'jiangsu',
-    label: 'Jiangsu',
-    children: [
-      {
-        value: 'nanjing',
-        label: 'Nanjing',
-        children: [
-          {
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-          },
-        ],
-      },
-    ],
-  },
-];
+
 
 const formItemLayout = {
   labelCol: {
@@ -71,166 +41,181 @@ const tailFormItemLayout = {
     },
   },
 };
-export default function Register() {
-    const [form] = Form.useForm();
 
-    const onFinish = (values: any) => {
-      console.log('Received values of form: ', values);
-    };
+export default function Register() {
+  const [form] = Form.useForm();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [location,setLocation] = useState("");
   
-    const prefixSelector = (
-      <Form.Item name="prefix" noStyle>
-        <Select style={{ width: 70 }}>
-          <Option value="86">+86</Option>
-          <Option value="87">+87</Option>
-        </Select>
-      </Form.Item>
-    );
-  
-    const suffixSelector = (
-      <Form.Item name="suffix" noStyle>
-        <Select style={{ width: 70 }}>
-          <Option value="USD">$</Option>
-          <Option value="CNY">¥</Option>
-        </Select>
-      </Form.Item>
-    );
-  
-    const [autoCompleteResult, setAutoCompleteResult] = useState<string[]>([]);
-  
-    const useOnWebsiteChange = (value: string) => {
-      if (!value) {
-        setAutoCompleteResult([]);
-      } else {
-        setAutoCompleteResult(['.com', '.org', '.net'].map(domain => `${value}${domain}`));
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select style={{ width: 70 }}>
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
+
+  const suffixSelector = (
+    <Form.Item name="suffix" noStyle>
+      <Select style={{ width: 70 }}>
+        <Option value="USD">$</Option>
+        <Option value="CNY">¥</Option>
+      </Select>
+    </Form.Item>
+  );
+
+  const [autoCompleteResult, setAutoCompleteResult] = useState<string[]>([]);
+
+  const useOnWebsiteChange = (value: string) => {
+    if (!value) {
+      setAutoCompleteResult([]);
+    } else {
+      setAutoCompleteResult(['.com', '.org', '.net'].map(domain => `${value}${domain}`));
+    }
+  };
+
+  const websiteOptions = autoCompleteResult.map(website => ({
+    label: website,
+    value: website,
+  }));
+  const handleComplete = (data: any) => {
+    let fullAddress = data.address;
+    let extraAddress = '';
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddress += data.bname;
       }
-    };
-  
-    const websiteOptions = autoCompleteResult.map(website => ({
-      label: website,
-      value: website,
-    }));
-  
-    return (
-      <Form
-        {...formItemLayout}
-        form={form}
-        name="register"
-        onFinish={onFinish}
-        initialValues={{
-          residence: ['zhejiang', 'hangzhou', 'xihu'],
-          prefix: '86',
-        }}
-        scrollToFirstError
+      if (data.buildingName !== '') {
+        extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+      }
+      fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
+      setLocation(fullAddress);
+      setIsModalVisible(false);
+    }
+  }
+  return (
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
       >
-        <Form.Item
-          name="email"
-          label="E-mail"
-          rules={[
-            {
-              type: 'email',
-              message: 'The input is not valid E-mail!',
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error('The two passwords that you entered do not match!'));
             },
-            {
-              required: true,
-              message: 'Please input your E-mail!',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-  
-        <Form.Item
-          name="password"
-          label="Password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-          hasFeedback
-        >
-          <Input.Password />
-        </Form.Item>
-  
-        <Form.Item
-          name="confirm"
-          label="Confirm Password"
-          dependencies={['password']}
-          hasFeedback
-          rules={[
-            {
-              required: true,
-              message: 'Please confirm your password!',
-            },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(new Error('The two passwords that you entered do not match!'));
-              },
-            }),
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-  
-        <Form.Item
-          name="nickname"
-          label="Nickname"
-          tooltip="What do you want others to call you?"
-          rules={[{ required: true, message: 'Please input your nickname!', whitespace: true }]}
-        >
-          <Input />
-        </Form.Item>
-  
-        <Form.Item
-          name="residence"
-          label="Habitual Residence"
-          rules={[
-            { type: 'array', required: true, message: 'Please select your habitual residence!' },
-          ]}
-        >
-          <Cascader options={residences} />
-        </Form.Item>
-  
-        <Form.Item
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="nickname"
+        label="닉네임"
+        tooltip="What do you want others to call you?"
+        rules={[{ required: true, message: 'Please input your nickname!', whitespace: true }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="location"
+        label="주소"
+        rules={[
+          { required: true, message: 'Please select your habitual residence!' },
+        ]}
+      >
+        <Input value={location}/>
+        <Button type="ghost" onClick={showModal}>
+          주소 찾기
+        </Button>
+        
+       
+      </Form.Item>
+
+      <Form.Item
+        name="place"
+        label="지점명"
+        rules={[
+          { required: true, message: 'Please select your habitual residence!' },
+        ]}
+      >
+        <Input placeholder='헬스장 이름' />
+      </Form.Item>
+      {/* <Form.Item
           name="phone"
           label="Phone Number"
           rules={[{ required: true, message: 'Please input your phone number!' }]}
         >
           <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
-        </Form.Item>
-  
-        <Form.Item
-          name="donation"
-          label="Donation"
-          rules={[{ required: true, message: 'Please input donation amount!' }]}
-        >
-          <InputNumber addonAfter={suffixSelector} style={{ width: '100%' }} />
-        </Form.Item>
-  
-        <Form.Item
-          name="website"
-          label="Website"
-          rules={[{ required: true, message: 'Please input website!' }]}
-        >
-          <AutoComplete options={websiteOptions} onChange={useOnWebsiteChange} placeholder="website">
-            <Input />
-          </AutoComplete>
-        </Form.Item>
-  
-        <Form.Item
-          name="intro"
-          label="Intro"
-          rules={[{ required: true, message: 'Please input Intro' }]}
-        >
-          <Input.TextArea showCount maxLength={100} />
-        </Form.Item>
-  
-        <Form.Item
+        </Form.Item> */}
+
+
+
+
+
+      {/* <Form.Item
           name="gender"
           label="Gender"
           rules={[{ required: true, message: 'Please select gender!' }]}
@@ -238,11 +223,10 @@ export default function Register() {
           <Select placeholder="select your gender">
             <Option value="male">Male</Option>
             <Option value="female">Female</Option>
-            <Option value="other">Other</Option>
           </Select>
-        </Form.Item>
-  
-        <Form.Item label="Captcha" extra="We must make sure that your are a human.">
+        </Form.Item> */}
+      {/* TODO: get domain  */}
+      {/* <Form.Item label="Captcha" extra="We must make sure that your are a human.">
           <Row gutter={8}>
             <Col span={12}>
               <Form.Item
@@ -257,28 +241,29 @@ export default function Register() {
               <Button>Get captcha</Button>
             </Col>
           </Row>
-        </Form.Item>
-  
-        <Form.Item
-          name="agreement"
-          valuePropName="checked"
-          rules={[
-            {
-              validator: (_, value) =>
-                value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-            },
-          ]}
-          {...tailFormItemLayout}
-        >
-          <Checkbox>
-            I have read the <a href="">agreement</a>
-          </Checkbox>
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Register
-          </Button>
-        </Form.Item>
-      </Form>
-    );
-  };
+        </Form.Item> */}
+
+      <Form.Item
+        name="agreement"
+        valuePropName="checked"
+        rules={[
+          {
+            validator: (_, value) =>
+              value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+          },
+        ]}
+        {...tailFormItemLayout}
+      >
+        <Checkbox>
+          I have read the <a href="">agreement</a>
+        </Checkbox>
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form.Item>
+
+    </Form>
+  );
+};
