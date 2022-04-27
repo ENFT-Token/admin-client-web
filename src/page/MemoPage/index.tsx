1511ab9e62b473023dab16852b36c6b01110b36a
Moonchop <moonchop913@gmail.com>
2022-04-27 13:03:11 +0900

    add : nameInput, bodyInput

--- src/page/MemoPage/index.tsx
+++ src/page/MemoPage/index.tsx
@@ -1,36 +1,62 @@
-import { Table, Badge, Menu, Dropdown, Space } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
+import { Table, Input, Button } from 'antd';
+import { useState,useRef } from 'react'
 
 
 
 
-
-export default function NestedTable(){
-
+export default function NestedTable() {
+  const [memoList, setMemoList] = useState([] as any);
+  const [inputBody, setInputBody] = useState([] as any);
+  const [inputHead, setInputHead] = useState([] as any);
+  const date = new Date();
+  //input 
+  const { TextArea } = Input;
+  const num = useRef(1);
+  const onChangeBody = (e: any) => {
+    setInputBody(e.target.value);
+  };
+  const onChangeHead = (e: any) => {
+    setInputHead(e.target.value);
+  };
+  const OnClickBtn = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (inputBody && inputHead) {
+      setMemoList([
+        ...memoList,
+        {
+          id : num.current++,
+          name: inputHead,
+          text: inputBody,
+          createdAt: date.toString().substr(0,25),
+        },
+      ]);
+      setInputHead("");
+      setInputBody("");
+    }
+  }
   const columns = [
     { title: 'Name', dataIndex: 'name', key: 'name' },
     { title: 'text', dataIndex: 'text', key: 'upgradeNum' },
-    { title: 'Creator', dataIndex: 'creator', key: 'creator' },
     { title: 'Date', dataIndex: 'createdAt', key: 'createdAt' },
-    { title: 'Action', key: 'operation', render: () => <a>Publish</a> },
+    { title: 'Action', key: 'operation', render: () => <button>Publish</button> },
   ];
 
-  const data = [];
-  for (let i = 0; i < 3; ++i) {
-    data.push({
-      key: i,
-      name: 'Screem',
-      text: 'iOsdsdsdsdsdsdsdsdssssssssssssssssdsdsdsdsdsdsdsdS',
-      creator: 'Jack',
-      createdAt: '2014-12-24 23:12:00',
-    });
-  }
-
   return (
-    <Table
-      className="components-table-demo-nested"
-      columns={columns}
-      dataSource={data}
-    />
+    <div>
+      <Table
+        className="components-table-demo-nested"
+        columns={columns}
+        dataSource={memoList}
+      />
+      <Input placeholder="작성자 이름" onChange={onChangeHead} value={inputHead}/>
+      <TextArea placeholder="내용" 
+      value={inputBody} showCount maxLength={100} autoSize={{ minRows: 5, maxRows: 5 }} onChange={onChangeBody} />
+      
+      <Button onClick={OnClickBtn}>확인</Button>
+      
+    </div >
   );
 }
+
+
+
+
