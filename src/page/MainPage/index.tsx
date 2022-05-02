d9e04371a53573ca3da1b88ee89c015a64d69ba8
Moonchop <moonchop913@gmail.com>
2022-05-02 14:08:37 +0900

    modify : layout at main

--- src/page/MainPage/index.tsx
+++ src/page/MainPage/index.tsx
@@ -4,17 +4,13 @@ import styled from "styled-components";
 import "./index.css"
 
 
-
-const { Header, Footer, Sider, Content } = Layout;
 export default function MainPage() {
   return (
     <div>
 
       <div className="container">
 
-        <div className="top">
-          <span>TOP (1000 x 80) <br /> #FF5E00;</span>
-        </div>
+      
 
 
         <div className="middle">
@@ -23,7 +19,7 @@ export default function MainPage() {
 
           <div className="middle-right">
 
-            <div className="middle-right-1"> <span>RIGHT1 (380 x 150) <br /> #FF00DD;</span> </div>
+            <div className="middle-right-1"> <span>관리자 정보<br /> #FF00DD;</span> </div>
             <div className="middle-right-2"> <span>RIGHT2 (380 x 150) <br /> #FFE400;</span> </div>
             <div className="middle-right-3"> <span>RIGHT3 (380 x 170) <br /> #99E000;</span> </div>
 
@@ -32,7 +28,6 @@ export default function MainPage() {
         </div>
 
         <div className="bottom"> <span>BOTTOM (1000 x 100) <br /> #5D5D5D;</span> </div>
-
       </div>
 
 
