d1a6e646c7f9382d368df66a9b61de0847a0fea6
Moonchop <moonchop913@gmail.com>
2022-05-07 01:47:10 +0900

    add : page count  button at approve test

--- src/page/ApprovePage/test.tsx
+++ src/page/ApprovePage/test.tsx
@@ -146,8 +146,18 @@ function Table({ columns, data }: any) {
                         >
                     </input>
                 </span>{' '}
-               
-            
+                <select
+                    value={pageSize}
+                    onChange={e =>{
+                        setPageSize(Number(e.target.value))
+                    }}
+                >
+                    {[10, 20, 30, 40,50].map(pageSize =>(
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
             </div>
         </div>
 
