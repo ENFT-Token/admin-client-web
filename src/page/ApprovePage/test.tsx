c56e4cc59ff3cddf20fe83f302c0332d84219f3e
Moonchop <moonchop913@gmail.com>
2022-05-07 01:44:27 +0900

    add : page button at approve test

--- src/page/ApprovePage/test.tsx
+++ src/page/ApprovePage/test.tsx
@@ -67,7 +67,7 @@ function Table({ columns, data }: any) {
             {
                 columns,
                 data,
-                initialState: { pageIndex: 2 },
+                initialState: { pageIndex: 0 },
             },
             usePagination
         )
@@ -114,6 +114,41 @@ function Table({ columns, data }: any) {
                     })}
                 </tbody>
             </table>
+            <div className="pagination">
+                <button onClick={()=> gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={()=> previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={()=> nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={()=> gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex+1} of {pageOptions.length}
+                    </strong>
+                </span>
+                <span>
+                    | Go to page:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(page)
+                        }}
+                        style = {{width : '100px'}}
+                        >
+                    </input>
+                </span>{' '}
+               
+            
+            </div>
         </div>
 
     )
