1b7d352188630a5cec0956c56589cc97a409ffc1
Moonchop <moonchop913@gmail.com>
2022-05-13 17:16:05 +0900

    modify : alarm css

--- src/page/MainPage/index.tsx
+++ src/page/MainPage/index.tsx
@@ -15,12 +15,14 @@ import MemberWidget from '../../widget/MemberWidget';
 import CheckCount from "../../widget/CheckCountWidget";
 import Alarm from "../../widget/AlarmWidget";
 
+
+import { MdChecklist } from "react-icons/md";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 export default function MainPage() {
   const admin = useSelector((store: Rootstate) => store.admin.adminInfo);
 
   const layout = [
-    { i: "현재", x: 0, y: 0, w: 3, h: 3, isResizable: false, minW: 3, maxW: 4  },
+    { i: "현재", x: 0, y: 0, w: 3, h: 3, isResizable: false, minW: 3, maxW: 4 },
     { i: "헬스장 이름", x: 3, y: 0, w: 5, h: 1.5, isResizable: false },
     { i: "관리자 정보", x: 8, y: 0, w: 4, h: 6, isResizable: false },
 
@@ -41,46 +43,50 @@ export default function MainPage() {
 
   return (
 
-      <div className="container">
-        <ResponsiveGridLayout
-          className="layout"
-          layouts={{lg: layout}}
-          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-          rowHeight={80}
-          width={1500}
-          margin={[25, 25]}
+    <div className="container">
+      <ResponsiveGridLayout
+        className="layout"
+        layouts={{ lg: layout }}
+        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+        cols={{ lg: 12, md: 12, sm: 12, xs: 8, xxs: 2 }}
+        rowHeight={80}
+        width={1500}
+        margin={[25, 25]}
 
-        >
+      >
 
-          <div key="관리자 정보" className="widget" id="adminInfo" >
-            <InfoWidget />
-          </div>
-          <div key="헬스장 이름" className="widget" id="title" >
-            <div >{admin?.place}</div>
-          </div>
-          <div key="현재 회원들" className="widget">
-            <MemberWidget />
-          </div>
-          <div key="광고1" className="widget">광고1</div>
-          <div key="광고2" className="widget">광고2</div>
-          <div key="현재" className="widget" id="checkin">
+        <div key="관리자 정보" className="widget" id="adminInfo" >
+          <InfoWidget />
+        </div>
+        <div key="헬스장 이름" className="widget" id="title" >
+          <div >{admin?.place}</div>
+        </div>
+        <div key="현재 회원들" className="widget">
+          <MemberWidget />
+        </div>
+        <div key="광고1" className="widget">광고1</div>
+        <div key="광고2" className="widget">광고2</div>
+        <div key="현재" className="widget" id="checkin">
+          <div id="checkinText">
             하루 누적 이용자 수 : x 명
             <CheckCount />
           </div>
-          <div key="알림" className="widget" id="alarm">
-            <h1 id="alarmTitle">알림</h1>
-            <div id="alarmText"><Alarm /></div>
+        </div>
+        <div key="알림" className="widget" id="alarm">
+          <div id="alarmText">
+            <h1 id="alarmHeader"><MdChecklist style={{marginRight:"15px"}}/>알림</h1>
+            <div id="alarmBody"><Alarm /></div>
           </div>
-          <div key="관리자 메모" className="widget" id="memo">
-            <MemoWidget />
-          </div>
-          <div key="접속 중인 회원들" className="widget" >
-            접속중인 회원들리스트 : 소켓이용
-          </div>
+        </div>
+        <div key="관리자 메모" className="widget" id="memo">
+          <MemoWidget />
+        </div>
+        <div key="접속 중인 회원들" className="widget" >
+          접속중인 회원들리스트 : 소켓이용
+        </div>
 
-        </ResponsiveGridLayout>
-      </div>
+      </ResponsiveGridLayout>
+    </div>
 
   );
 }
--- src/widget/AlarmWidget/index.tsx
+++ src/widget/AlarmWidget/index.tsx
@@ -1,7 +1,8 @@
 import React, { useMemo } from 'react'
 import { useSelector } from 'react-redux';
 import { Rootstate } from '../../models';
-
+import { FcCheckmark } from "react-icons/fc";
+import styled from 'styled-components';
 export default function Alarm() {
     const list = useMemo(() => {
         const bodyString = window.localStorage.getItem("memo");
@@ -11,8 +12,19 @@ export default function Alarm() {
 
     return (
         <div>
-            <div>현재 관리자 메모가 {list}개 있습니다</div>
-            <div>현재 승인 요청한 유저가 {userCount}명입니다.</div>
+            <div><FcCheckmark style={{ marginRight: "15px" }} />현재 관리자 메모가 <Text>{list} 개</Text> 있습니다</div>
+            <div><FcCheckmark style={{ marginRight: "15px" }} />현재 승인 요청한 유저가 <Text>{userCount} 명</Text> 입니다.</div>
         </div>
     )
 }
+
+
+
+
+const Text = styled.span`
+        color:white;
+        border:10px;
+        border-radius: 5px;
+        background-color: #009416;
+        padding:5px 10px;
+`
