9574c49a42c1fa258e895a57cc5264ba310708ea
gron1gh1 <gron1gh1@gmail.com>
2022-05-20 02:10:13 +0900

    fix: localStorage memo init

--- src/page/MainPage/index.tsx
+++ src/page/MainPage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Layout } from "antd";
 import styled from "styled-components";
 import "./index.css";
@@ -7,14 +7,13 @@ import GridLayout from "react-grid-layout";
 import { Responsive, WidthProvider } from "react-grid-layout";
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
-import InfoWidget from '../../widget/InfoWidget';
-import MemoWidget from '../../widget/MemoWidget';
-import { Rootstate } from '../../models';
-import { useSelector } from 'react-redux';
-import MemberWidget from '../../widget/MemberWidget';
+import InfoWidget from "../../widget/InfoWidget";
+import MemoWidget from "../../widget/MemoWidget";
+import { Rootstate } from "../../models";
+import { useSelector } from "react-redux";
+import MemberWidget from "../../widget/MemberWidget";
 import CheckCount from "../../widget/CheckCountWidget";
 import Alarm from "../../widget/AlarmWidget";
-
 
 import { MdChecklist } from "react-icons/md";
 import Footer from "../../widget/Footer/Footer";
@@ -31,20 +30,13 @@ export default function MainPage() {
     { i: "알림", x: 0, y: 0, w: 3, h: 3, isResizable: false },
     { i: "현재 회원들", x: 3, y: 0, w: 5, h: 5.5, isResizable: false },
 
-
     { i: "접속 중인 회원들", x: 0, y: 0, w: 3, h: 4, isResizable: false },
     { i: "관리자 메모", x: 3, y: 0, w: 5, h: 3, isResizable: false },
     { i: "광고1", x: 8, y: 0, w: 4, h: 2, isResizable: false },
     { i: "광고2", x: 8, y: 0, w: 4, h: 2, isResizable: false },
-
-
-
-
-
   ];
 
   return (
-
     <div className="container">
       <ResponsiveGridLayout
         className="layout"
@@ -54,20 +46,22 @@ export default function MainPage() {
         rowHeight={80}
         width={1500}
         margin={[25, 25]}
-
       >
-
-        <div key="관리자 정보" className="widget" id="adminInfo" >
+        <div key="관리자 정보" className="widget" id="adminInfo">
           <InfoWidget />
         </div>
-        <div key="헬스장 이름" className="widget" id="title" >
-          <div >{admin?.place}</div>
+        <div key="헬스장 이름" className="widget" id="title">
+          <div>{admin?.place}</div>
         </div>
         <div key="현재 회원들" className="widget">
           <MemberWidget />
         </div>
-        <div key="광고1" className="widget">광고1</div>
-        <div key="광고2" className="widget">광고2</div>
+        <div key="광고1" className="widget">
+          광고1
+        </div>
+        <div key="광고2" className="widget">
+          광고2
+        </div>
         <div key="현재" className="widget" id="checkin">
           <div id="checkinText">
             하루 누적 이용자 수 : x 명
@@ -76,22 +70,24 @@ export default function MainPage() {
         </div>
         <div key="알림" className="widget" id="alarm">
           <div id="alarmText">
-            <h1 id="alarmHeader">알림<MdChecklist style={{marginLeft:"15px"}}/></h1>
-            <div id="alarmBody"><Alarm /></div>
+            <h1 id="alarmHeader">
+              알림
+              <MdChecklist style={{ marginLeft: "15px" }} />
+            </h1>
+            <div id="alarmBody">
+              <Alarm />
+            </div>
           </div>
         </div>
         <div key="관리자 메모" className="widget" id="memo">
           <MemoWidget />
         </div>
-        <div key="접속 중인 회원들" className="widget" >
+        <div key="접속 중인 회원들" className="widget">
           접속중인 회원들리스트 : 소켓이용
         </div>
-
       </ResponsiveGridLayout>
 
-      <Footer/>
+      <Footer />
     </div>
-
   );
 }
-
--- src/widget/AlarmWidget/index.tsx
+++ src/widget/AlarmWidget/index.tsx
@@ -1,32 +1,45 @@
-import React, { useMemo } from 'react'
-import { useSelector } from 'react-redux';
-import { Rootstate } from '../../models';
+import React, { useEffect, useMemo } from "react";
+import { useSelector } from "react-redux";
+import { Rootstate } from "../../models";
 import { FcCheckmark } from "react-icons/fc";
-import styled from 'styled-components';
+import styled from "styled-components";
 export default function Alarm() {
-    const list = useMemo(() => {
-        const bodyString = window.localStorage.getItem("memo");
-        return JSON.parse(bodyString as string).list.length;
-    }, []);
-    const userCount = useSelector((store: Rootstate) => store.members.approvedUser.length)
+  const list = useMemo(() => {
+    let bodyString = window.localStorage.getItem("memo");
+    if (!bodyString) {
+      const tempObjBody = JSON.stringify({ num: 0, list: [] });
+      window.localStorage.setItem("memo", tempObjBody);
+      bodyString = tempObjBody;
+    }
+    return JSON.parse(bodyString as string).list.length;
+  }, []);
+  const userCount = useSelector(
+    (store: Rootstate) => store.members.approvedUser.length
+  );
 
-    return (
-        <Form>
-            <div><FcCheckmark style={{ marginRight: "15px" }} />현재 관리자 메모가 <Text>{list} 개</Text> 있습니다</div>
-            <div><FcCheckmark style={{ marginRight: "15px" }} />현재 승인 요청한 유저가 <Text>{userCount} 명</Text> 입니다.</div>
-        </Form>
-    )
+  return (
+    <Form>
+      <div>
+        <FcCheckmark style={{ marginRight: "15px" }} />
+        현재 관리자 메모가 <Text>{list} 개</Text> 있습니다
+      </div>
+      <div>
+        <FcCheckmark style={{ marginRight: "15px" }} />
+        현재 승인 요청한 유저가 <Text>{userCount} 명</Text> 입니다.
+      </div>
+    </Form>
+  );
 }
 
-
-
 const Form = styled.div`
-    div{margin-bottom:20px};
-`
+  div {
+    margin-bottom: 20px;
+  }
+`;
 const Text = styled.span`
-        color:white;
-        border:10px;
-        border-radius: 5px;
-        background-color: #009416;
-        padding:5px 10px;
-`
+  color: white;
+  border: 10px;
+  border-radius: 5px;
+  background-color: #009416;
+  padding: 5px 10px;
+`;
