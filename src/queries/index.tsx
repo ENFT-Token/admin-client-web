9ef27b4907bd36101e3bd275f3375a8e805cd104
gron1gh1 <gron1gh1@gmail.com>
2022-05-30 16:46:42 +0900

    add: api list

--- src/queries/index.tsx
+++ src/queries/index.tsx
@@ -1,47 +1,45 @@
-import {QueryClient} from "react-query";
-import {RequestAuth} from "../models/Request";
+import { QueryClient } from "react-query";
+import { RequestAuth } from "../models/Request";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 queryClient.setQueryDefaults("approveList", {
-    queryFn: () => RequestAuth("GET", "/admin/approve/list"),
-    select: (response) => {
-        return response.data;
-    }
+  queryFn: () => RequestAuth("GET", "/admin/approve/list"),
+  select: (response) => {
+    return response.data;
+  },
+  refetchInterval: 3000,
 });
 
 queryClient.setQueryDefaults("memberList", {
-    queryFn: () => RequestAuth("GET", "/admin/member"),
-    select: (response) => {
-        return response.data;
-    }
+  queryFn: () => RequestAuth("GET", "/admin/member"),
+  select: (response) => {
+    return response.data;
+  },
+  refetchInterval: 3000,
 });
 
 queryClient.setQueryDefaults("todayCount", {
-    queryFn: () => RequestAuth("GET", "/check/today_count"),
-    select: (response) => {
-        return response?.data?.count ?? 0;
-    },
-    refetchInterval: 3000,
+  queryFn: () => RequestAuth("GET", "/check/today_count"),
+  select: (response) => {
+    return response?.data?.count ?? 0;
+  },
+  refetchInterval: 3000,
 });
-
-
 
 queryClient.setQueryDefaults("check", {
-    queryFn: () => RequestAuth("GET", "/check"),
-    select: (response) => {
-        return response.data;
-    },
-    refetchInterval: 3000,
+  queryFn: () => RequestAuth("GET", "/check"),
+  select: (response) => {
+    return response.data;
+  },
+  refetchInterval: 3000,
 });
 
-
-
 queryClient.setQueryDefaults("priceInfo", {
-    queryFn: () => RequestAuth("GET", "/admin/priceInfo"),
-    select: (response) => {
-        return response.data;
-    },
+  queryFn: () => RequestAuth("GET", "/admin/priceInfo"),
+  select: (response) => {
+    return response.data;
+  },
 });
 
 export default queryClient;
