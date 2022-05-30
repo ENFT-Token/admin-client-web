1c60d0b39f6e7086be98d83b1c9a6e883b4d3dbc
gron1gh1 <gron1gh1@gmail.com>
2022-05-30 14:02:16 +0900

    add: MemberPage 만료일 추가

--- src/page/MembersPage/index.tsx
+++ src/page/MembersPage/index.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useMemo, useState } from "react";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Rootstate } from "../../models";
 import { SERVER_URL } from "../../confing";
 import Table, { Title } from "../../widget/TableWidget";
 import { RequestAuth } from "../../models/Request";
 import { Profile } from "../ApprovePage";
-import {useQuery} from "react-query";
-
+import { useQuery } from "react-query";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 export interface IListData {
   //렌더링계속되므로 함수밖에 작성
   address: string;
@@ -17,8 +18,9 @@ export interface IListData {
 }
 
 export default function MembersPage() {
-  const {data: members} = useQuery<Record<string, string>[]>("memberList");
-
+  const { data: members } = useQuery<Record<string, string>[]>("memberList");
+  const admin = useSelector((store: Rootstate) => store.admin.adminInfo);
+  const [ticketInfo, setTicketInfo] = useState<Record<string, any>>({});
   const columns = useMemo(
     () => [
       {
@@ -31,7 +33,7 @@ export default function MembersPage() {
       },
       {
         Header: "만료일",
-        accessor: "requestDay",
+        accessor: "endDay",
       },
       {
         Header: "성별",
@@ -41,24 +43,52 @@ export default function MembersPage() {
         Header: "지갑 주소",
         accessor: "address",
       },
-  
     ],
     []
   );
 
-  const data = useMemo(
-    () =>
-        members?.map((v) => {
-      return{
-        profile : (<Profile  width="60" height="60" src={`http://${SERVER_URL}${v.profile}`}></Profile>),
-        nickname: v.nickname,
-        //requestDay: v.requsetDay,
-        sex: v.sex,
-        address: v.address,
-      }
-      }) ?? [],
-    [members]
-  );
+  useEffect(() => {
+    if (members) {
+      // 멤버가 소유한 티켓 조회하고 데이터 꺼내기
+      const fetch = async () => {
+        const data: Record<string, any> = {};
+        for (const { address } of members) {
+          const response = await axios.get(`http://${SERVER_URL}/user/myNFT`, {
+            params: {
+              address,
+            },
+          });
+          console.log(response.data);
+          data[address] = response.data
+            .map((token: string) => jwt_decode(token))
+            .find((v: any) => v.place === admin?.place);
+        }
+        setTicketInfo(data);
+      };
+      fetch();
+    }
+  }, [members]);
+
+  const data = useMemo(() => {
+    return (
+      members?.map((v) => {
+        return {
+          profile: (
+            <Profile
+              width="60"
+              height="60"
+              src={`http://${SERVER_URL}${v.profile}`}
+            ></Profile>
+          ),
+          endDay: ticketInfo[v.address]?.end_date ?? "-",
+          nickname: v.nickname,
+          //requestDay: v.requsetDay,
+          sex: v.sex,
+          address: v.address,
+        };
+      }) ?? []
+    );
+  }, [members, ticketInfo]);
 
   if (!members) {
     return <div>Loading...</div>;
@@ -73,5 +103,3 @@ export default function MembersPage() {
     </div>
   );
 }
-
-
